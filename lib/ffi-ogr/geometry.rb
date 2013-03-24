40009b52f17b6830fcf937d2b7b473b8e48ae18a
Scooter Wadsworth <scooterwadsworth@gmail.com>
2013-03-24 11:03:14 -0600

    add additional functions to geometry class

--- lib/ffi-ogr/geometry.rb
+++ lib/ffi-ogr/geometry.rb
@@ -13,6 +13,21 @@ module OGR
       FFIOGR.OGR_G_GetGeometryType(@ptr)
     end
 
+    def get_length
+      FFIOGR.OGR_G_Length(@ptr)
+    end
+    alias_method :length, :get_length
+
+    def get_area
+      FFIOGR.OGR_G_Area(@ptr)
+    end
+    alias_method :area, :get_area
+
+    def get_boundary
+      FFIOGR.OGR_G_Boundary(@ptr)
+    end
+    alias_method :boundary, :get_boundary
+
     def to_geojson
       MultiJson.load(FFIOGR.OGR_G_ExportToJson(@ptr))
     end
@@ -20,6 +35,10 @@ module OGR
     def to_kml(elevation=nil)
       elevation = String(elevation) unless elevation.nil?
       FFIOGR.OGR_G_ExportToKML(@ptr, elevation)
+    end
+
+    def to_gml
+      FFIOGR.OGR_G_ExportToGML(@ptr)
     end
   end
 end
--- lib/ffi-ogr/point.rb
+++ lib/ffi-ogr/point.rb
@@ -1,5 +1,19 @@
 module OGR
   class Point < Geometry
     # Point
+    def get_x
+      FFIOGR.OGR_G_GetX(@ptr)
+    end
+    alias_method :x, :get_x
+
+    def get_y
+      FFIOGR.OGR_G_GetY(@ptr)
+    end
+    alias_method :y, :get_y
+
+    def get_z
+      FFIOGR.OGR_G_GetZ(@ptr)
+    end
+    alias_method :z, :get_z
   end
 end
