0160c1a919238f48a6a7f9de701484ecfadb26ac
Scooter Wadsworth <scooterwadsworth@gmail.com>
2013-11-07 23:06:05 -0700

    Update tests; Add simplecov

--- spec/data_source_spec.rb
+++ spec/data_source_spec.rb
@@ -1,3 +1,4 @@
+require File.expand_path 'spec/spec_helper'
 require 'rspec/expectations'
 require File.expand_path 'lib/ffi-ogr'
 require 'fileutils'
@@ -13,7 +14,7 @@ describe OGR::DataSource do
   it "should convert to CSV" do
     csv_file_name = './spec/data/ds_csv_test.csv'
 
-    ds = OGR::GenericReader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
+    ds = OGR::Reader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
 
     csv = ds.to_csv csv_file_name
     File.exist?(csv_file_name).should be true
@@ -22,7 +23,7 @@ describe OGR::DataSource do
   it "should convert to KML" do
     kml_file_name = './spec/data/ds_kml_test.csv'
 
-    ds = OGR::GenericReader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
+    ds = OGR::Reader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
 
     kml = ds.to_kml kml_file_name
     File.exist?(kml_file_name).should be true
@@ -33,7 +34,7 @@ describe OGR::DataSource do
     shp_file_name = './spec/data/ds_shp_test/ds_shp_test.shp'
     shx_file_name = './spec/data/ds_shp_test/ds_shp_test.shx'
 
-    ds = OGR::GenericReader.new('geojson').read './spec/data/ne_110m_coastline.geojson'
+    ds = OGR::Reader.new('geojson').read './spec/data/ne_110m_coastline.geojson'
 
     shp = ds.to_shp shp_file_name
     (File.exist?(shp_file_name) && File.exist?(shx_file_name)).should be true
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,19 +1,20 @@
+require File.expand_path 'spec/spec_helper'
 require File.expand_path 'lib/ffi-ogr'
 
-describe OGR::GenericReader do
+describe OGR::Reader do
 
   it "should create datasource from GeoJSON file" do
-    ds = OGR::GenericReader.new('geojson').read './spec/data/ne_110m_coastline.geojson'
+    ds = OGR::Reader.new('geojson').read './spec/data/ne_110m_coastline.geojson'
     ds.class.should eq OGR::DataSource
   end
 
   it "should create datasource from shapefile" do
-    ds = OGR::GenericReader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
+    ds = OGR::Reader.new('ESRI Shapefile').read './spec/data/ne_110m_coastline/ne_110m_coastline.shp'
     ds.class.should eq OGR::DataSource
   end
 
   it "should create datasource from KML file" do
-    ds = OGR::GenericReader.new('kml').read './spec/data/ne_110m_coastline.kml'
+    ds = OGR::Reader.new('kml').read './spec/data/ne_110m_coastline.kml'
     ds.class.should eq OGR::DataSource
   end
 
